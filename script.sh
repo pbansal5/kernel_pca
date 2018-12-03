48a5d78b82a9eee79f561786d667049bcae4c674
pbansal5 <parikshitb52@gmail.com>
2018-12-03 16:47:02 +0530

    tested using different C in linear SVM

--- script.sh
+++ script.sh
@@ -2,4 +2,3 @@
 python3 kernel_pca.py
 python3 test.py
 python3 svm.py
-python3 result.py
--- svm.py
+++ svm.py
@@ -10,8 +10,5 @@ with open("test.pkl","rb") as file:
 
 test_components = np.transpose(test_components)
 clf = svm.LinearSVC(multi_class="crammer_singer").fit(train_components,train_labels)
-out = clf.predict(test_components)
 
-with open("out",'wb') as file:
-    pickle.dump([out,test_labels],file)
-    
+print ((1-clf.score(test_components,test_labels))*100)
